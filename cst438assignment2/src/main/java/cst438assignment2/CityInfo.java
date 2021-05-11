531ee723934ea87f3ad22fbe27c031a367ddfba8
Robert Meis <rmeis@safesplash.com>
2021-05-10 19:14:46 -0600

    Added CityServiceTest

--- cst438assignment2/src/main/java/cst438assignment2/CityInfo.java
+++ cst438assignment2/src/main/java/cst438assignment2/CityInfo.java
@@ -17,4 +17,46 @@ public class CityInfo {
 		this.timeAndTemp = timeAndTemp;
 	}
 
+	@Override
+	public int hashCode()
+	{
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((city == null) ? 0 : city.hashCode());
+		result = prime * result + ((country == null) ? 0 : country.hashCode());
+		result = prime * result + ((timeAndTemp == null) ? 0 : timeAndTemp.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CityInfo other = (CityInfo) obj;
+		if (city == null)
+		{
+			if (other.city != null)
+				return false;
+		} else if (!city.equals(other.city))
+			return false;
+		if (country == null)
+		{
+			if (other.country != null)
+				return false;
+		} else if (!country.equals(other.country))
+			return false;
+		if (timeAndTemp == null)
+		{
+			if (other.timeAndTemp != null)
+				return false;
+		} else if (!timeAndTemp.equals(other.timeAndTemp))
+			return false;
+		return true;
+	}
+
 }
--- cst438assignment2/src/test/java/cst438assignment2/CityServiceTest.java
+++ cst438assignment2/src/test/java/cst438assignment2/CityServiceTest.java
@@ -1,6 +1,73 @@
 package cst438assignment2;
 
-public class CityServiceTest
-{
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+@SpringBootTest
+public class CityServiceTest {
+	
+	//Use MockBean to stub out classes required by CityController
+	@MockBean
+	private CityRepository cityRepository;
+	
+	@MockBean
+	private CountryRepository countryRepository;
+	
+	@MockBean
+	private WeatherService weatherService;
+	
+	//this is the class being tested
+	CityService cityService = new CityService();
+	
+	@BeforeEach
+	public void setUpEach() {
+		MockitoAnnotations.initMocks(this);
+	}
+	
+	//Test Methods
+	@Test
+	public void cityServiceShouldReturnValidCityInfoObject() {
+		
+		//create data to be returned by MockBeans
+		Country country = new Country("TST", "Test Country");
+		City city = new City(1, "TestCity", "TestDistrict", 1000, country);
+		List<City> cities = new ArrayList<City>();
+		cities.add(city);
+		
+		//create stub for database access via CityRepository
+		given(cityRepository.findByName("TestCity")).willReturn(cities);
+		
+		//create stub for database access via CityRepository
+		given(countryRepository.findByCode("TST")).willReturn(country);
+		
+		//create stub for WeatherService
+		given(weatherService.getTimeAndTemp("TestCity")).willReturn(new TimeAndTemp(273.15, 1000, -1000));
+		
+		//Expected TimeAndTemp object
+		TimeAndTemp expectedTimeAndTemp = new TimeAndTemp(273.15, 1000, -1000);
+		CityInfo expectedResult = new CityInfo(city, country, expectedTimeAndTemp);
+		
+		//Result from test
+		CityInfo result = cityService.getCityInfo("TestCity");
+		
+		//assert expected result = test result
+		//MUST implement .equals() method for CityInfo class
+		assertThat(expectedResult).isEqualTo(expectedResult);
+	}
+	
 }
